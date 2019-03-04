c9bb1b43b509c15836d26499b3056eb416bc4084
alyoshz <alessiomalu@outlook.com>
2019-03-05 00:18:17 +0100

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,30 @@
 import React, {Component} from 'react'
 import { Link } from 'react-router-dom'
 
+import content from '../../content/footer.js'
+
 class Footer extends Component {
 
   render() {
     return (
       <footer className={'footer'}>
-        Footer
+
+        <div className={'footer__menu'}>
+          {content.menu.map((column, index) =>
+            <div key={index} className={"footer__menu__column"}>
+              <div className={"footer__heading"}>{column.title}</div>
+              <ul>
+                {column.links.map((link, index) =>
+                  <li key={index}>
+                    <Link to={link.url}>{link.text}</Link>
+                  </li>
+                )}
+              </ul>
+            </div>
+          )}
+        </div>
+
+        <div className={'footer__copyright'}>{content.copyright}</div>
       </footer>
     );
   }
