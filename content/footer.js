47d861ff460efcbb82acaebd0ab83b218d0944a6
alyoshz <alessiomalu@outlook.com>
2019-03-05 00:23:33 +0100

    Footer contents

---
+++ content/footer.js
@@ -0,0 +1,64 @@
+module.exports = {
+  menu: [
+
+    // First
+    {
+      title: 'Title',
+      links: [
+        {
+          url: 'https://google.com',
+          text: 'Link'
+        },
+        {
+          url: 'https://google.com',
+          text: 'Link'
+        },
+        {
+          url: 'https://google.com',
+          text: 'Link'
+        },
+      ],
+    },
+
+    // Second
+    {
+      title: 'Title',
+      links: [
+        {
+          url: 'https://google.com',
+          text: 'Link'
+        },
+        {
+          url: 'https://google.com',
+          text: 'Link'
+        },
+        {
+          url: 'https://google.com',
+          text: 'Link'
+        },
+      ],
+    },
+
+    // Third
+    {
+      title: 'Title',
+      links: [
+        {
+          url: 'https://google.com',
+          text: 'Link'
+        },
+        {
+          url: 'https://google.com',
+          text: 'Link'
+        },
+        {
+          url: 'https://google.com',
+          text: 'Link'
+        },
+      ],
+    },
+
+  ],
+
+  copyright: 'Instaweb..'
+};
