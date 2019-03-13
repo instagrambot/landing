4cd7bbdd90ed099987986e6b68f19fb80133299d
alyoshz <alessiomalu@outlook.com>
2019-03-13 21:08:32 +0100

    Basic features

---
+++ content/features.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    icon: 'TODO',
+    title: 'Liking',
+    subtitle: 'Like posts by hashtag or username'
+  },
+  {
+    icon: 'TODO',
+    title: 'Following',
+    subtitle: 'Follow followers, following or who post to hashtag'
+  },
+  {
+    icon: 'TODO',
+    title: 'Scraping',
+    subtitle: 'Easily scrape info like followers/following list'
+  },
+  {
+    icon: 'TODO',
+    title: 'Updating',
+    subtitle: 'New ideas are in development, tell us yours'
+  },
+];
---
+++ src/components/Home/Features.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from 'react'
+import { Link } from 'react-router-dom'
+
+import { FaCheck } from 'react-icons/fa'
+
+import content from '../../../content/features.js'
+
+class Features extends Component {
+
+  render() {
+    return (
+      <div className={'features'}>
+        {content.map((feature, index) =>
+          <div key={index} className={"feature"}>
+            <div className={"icon"}><FaCheck /></div>
+            <div className={"title"}>{feature.title}</div>
+            <div className={"subtitle"}>{feature.subtitle}</div>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Features;
