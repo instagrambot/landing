8782586ecba035fc19a20b3f19c192d7a5183ed1
alyoshz <alessiomalu@outlook.com>
2019-03-18 17:54:07 +0100

    Tasks content

---
+++ content/tasks.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    title: 'Title',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.',
+    link: {
+      url: '#',
+      text: 'Click me'
+    },
+  },
+  {
+    title: 'Title',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.',
+    link: {
+      url: '#',
+      text: 'Click me'
+    },
+    checked: 'true'
+  },
+  {
+    title: 'Title',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et.',
+    link: {
+      url: '#',
+      text: 'Click me'
+    },
+  },
+];
